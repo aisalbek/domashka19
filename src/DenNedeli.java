77543a0051401a93d0fad7fd2b36d0488c464b60
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-05 14:27:30 +0600

    ff

--- src/DenNedeli.java
+++ src/DenNedeli.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public enum DenNedeli {
+public enum DenNedeli {
+    DUISHOMBY ("--техникалык сабак болот окууйм.--"),
+    SHEISHEMBI("--практикалык сабак болот окууйм--."),
+    SHARSHEMBI("--англис тил сабагы окууйм.--"),
+    BEISHEMBI("--практика окууйм.--"),
+    JUMA("--жума марек окууйм.--"),
+    ISHEMBI("--демалыш калочуларга окуу.--"),
+    JEKSHEMBI("------эс алам.!------");
+    private final String name;
+    DenNedeli(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Scanner scanner =new Scanner(System.in);
+        while (true) {
+            System.out.print("напишите день недели >>>");
+            String sk =scanner.nextLine().toUpperCase();
+            switch (sk) {
+                case "DUISHOMBY" -> System.out.println(DenNedeli.DUISHOMBY.getName());
+                case "SHEISHEMBI"-> System.out.println(DenNedeli.SHEISHEMBI.getName());
+                case "SHARSHEMBI"-> System.out.println(DenNedeli.SHARSHEMBI.getName());
+                case "BEISHEMBI" -> System.out.println(DenNedeli.BEISHEMBI.getName());
+                case "JUMA"      -> System.out.println(DenNedeli.JUMA.getName());
+                case "ISHEMBI"   -> System.out.println(DenNedeli.ISHEMBI.getName());
+                case "JEKSHEMBI" -> System.out.println(DenNedeli.JEKSHEMBI.getName());
+                default          -> System.out.println("!!==мындай кун жумада жок==!!");
+            }
+           }
+        }
+     }
+
